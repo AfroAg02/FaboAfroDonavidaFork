d4dbe15e4575c9a61868f0e49cfaf3a79b5ebea7
FaboReyes <faboale002@gmail.com>
2023-08-10 03:13:14 -0400

    falta lo del casteo

--- backend/api/controllers/meta.js
+++ backend/api/controllers/meta.js
@@ -2,35 +2,36 @@ const Meta = require('../models/Meta');
 const Rol = require('../models/Rol');
 
 
-exports.post = (req,resp) => {
+exports.post = async(req,resp) => {
     //valido que el rol no este vacio
-    const {rol,Components} = req.body;
+    const {rol,componentes} = req.body;
     if(!rol){
        return resp.status(400).json('Rol no valido');
     }
     //Valido la existencia de ese rol
-    const rolexist = Rol.exists({name:rol.title});
+    const rolexist = await Rol.exists({name:rol});
     if(!rolexist){
+        console.log("entra al if")
        return resp.status(400).json('Este rol no existe');
     }
-    let newMeta;
     Meta.findOne({rol:rol})
     .then(meta => {
         if(meta){
-           Meta.updateOne({rol:rol},{Componentes: Components});
+            console.log("entra al 2 if")
+           Meta.updateOne({rol:rol},{componentes :componentes});
         }else{
-        newMeta = new Meta({
+            console.log("entra al else")
+            meta = new Meta({
             rol: req.body.rol,
-            Components :req.body.Components
+            componentes :req.body.componentes
         });
-        newMeta.save();
+        meta.save();
         resp.json({
             success:true,
             message:"Fue creado la plantilla",
-            meta: newMeta
+            meta: meta
         });
         }
-
     })
     .catch(err =>{
         resp.status(400).json("Error en la creacion" + err);
