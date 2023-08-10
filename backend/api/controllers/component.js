7ec0a1db32d8eaf896b87ad9ebf05485565be256
FaboReyes <faboale002@gmail.com>
2023-08-10 01:39:17 -0400

    Arreglo post de component

--- backend/api/controllers/component.js
+++ backend/api/controllers/component.js
@@ -3,22 +3,25 @@ const Component = require('../models/Component');
 
 exports.post = (req,res) =>{
     const{etiqueta,name,type} = req.body;
-    const valor = requ.body.value;
+    const valor = req.body.value;
     if(!etiqueta || !name || !type){
+        console.log("entra al if")
         return res.status(400).json("Parametros incompletos");
     }
-    Component.finOne({etiqueta:etiqueta})
+    Component.findOne({etiqueta:etiqueta})
     .then(component => {
         if(component){
+            console.log("entra al if 2")
             return res.status(400).json("Componente ya existente");
         }else{
+            console.log("entra al else")
             component  = new Component({
                 etiqueta:etiqueta,
                 name:name,
                 type:type,
                 value:valor
             });
-            componet.save();
+            component.save();
             res.json({
                 success:true,
                 message:"Componente creado excitosament",
@@ -27,6 +30,7 @@ exports.post = (req,res) =>{
         }
     })
     .catch(err =>{
+        console.log("entra al error")
         res.json("Ha ocurrido un error " + err);
     });
 };
